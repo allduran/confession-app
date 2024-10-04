cb5b5673815cda4425c2cfa4f9edf6118b72d01d
duran <cheshire.dd96@gmail.com>
2024-10-04 13:45:27 +0200

    added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import Sidebar from './components/Layout/Sidebar';
-import Content from './components/Layout/Content';
-import BibleVerses from './components/Layout/BibleVerses';
-import confessionData from './data/confession.json';
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Layout/Sidebar";
+import Content from "./components/Layout/Content";
+import BibleVerses from "./components/Layout/BibleVerses";
+import Header from "./components/Common/Header";
+import confessionData from "./data/confession.json";
 
 function App() {
   const [selectedSection, setSelectedSection] = useState([]);
@@ -13,11 +14,23 @@ function App() {
   };
 
   return (
-    <div className="container">
-        <Sidebar chapters={confessionData.confesion.capitulos} onSectionClick={handleSectionClick} />
+    <>
+      <Header />
+      <div className="container">
+        <Sidebar
+          chapters={confessionData.confesion.capitulos}
+          onSectionClick={handleSectionClick}
+        />
         <Content sections={selectedSection} />
-        {selectedSection.length > 0 && <BibleVerses verseReferences={selectedSection.flatMap(section => section.referencias)} />}
-    </div>
+        {selectedSection.length > 0 && (
+          <BibleVerses
+            verseReferences={selectedSection.flatMap(
+              (section) => section.referencias
+            )}
+          />
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Common/Header.jsx
@@ -0,0 +1,11 @@
+import '../../styles/Header.css';
+
+const Header = () => {
+    return (
+      <header className="header">
+        <h1>Confesión Bautista de Fe de 1689</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
